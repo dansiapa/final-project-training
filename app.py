10f0712a0211a6838dfd565813cbae56e7a03f64
Rangga Putra <ranggaputra12343@gmail.com>
2026-04-29 15:42:46 +0000

    updated: disable debug mode

--- app.py
+++ app.py
@@ -187,4 +187,4 @@ def predict():
     except Exception as e: return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080,debug=False)
